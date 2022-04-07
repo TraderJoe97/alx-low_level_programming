2a99e9ce1f4d3c8afffaacbb23d2259d3b99f564
TraderJoe97 <jimonakedi@gmail.com>
2022-04-07 14:50:00 +0200

    fix task6 stdout

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	fprintf(stderr,"size of a char: %d byte(s)\n", sizeof(char));
-	fprintf(stderr,"size of an int: %d byte(s)\n", sizeof(int));
-	fprintf(stderr,"size of a long int: %d byte(s)\n", sizeof(long int));
-	fprintf(stderr,"size of a long long int: %d byte(s)\n", sizeof(long long int));
-	fprintf(stderr,"size of a float: %d byte(s)\n", sizeof(float));
+	fprintf(stdout, "size of a char: %d byte(s)\n", sizeof(char));
+	fprintf(stdout, "size of an int: %d byte(s)\n", sizeof(int));
+	fprintf(stdout, "size of a long int: %d byte(s)\n", sizeof(long int));
+	fprintf(stdout, "size of a long long int: %d byte(s)\n", sizeof(long long int));
+	fprintf(stdout, "size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
