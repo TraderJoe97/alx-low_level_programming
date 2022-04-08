772defd0c1b40ee7e296503ed0dbabc1492dfd41
TraderJoe97 <jimonakedi@gmail.com>
2022-04-08 19:54:42 +0200

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: always 0 (success/correct)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (j >= i)
+			{
+				putchar(i);
+				putchar(j);
+				for (k = 48; k <= 57; k++)
+				{
+					 for (l = 48; l<= 57; l++)
+					 putchar(k);
+					 putchar(l);
+				}
+				 
+
+				if ((i != 56) || (j != 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
