7626e24c36ac7156fbd9a32ed6cca8f51ae61ed5
TraderJoe97 <jimonakedi@gmail.com>
2022-07-28 21:00:29 +0200

    fix function declaration in header file for task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - function taht creates a hash table. 
+ * @size: size of the array 
+ * Return: pointer to the newly created hash table
+ * If something went wrong, return NULL
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *ht;
