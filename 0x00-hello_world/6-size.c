beab6bebcf2f9cc623377047e30fb1d71c0ef1cc
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2023-03-17 11:49:38 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ int main()
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	int ret = 0;
+	return (ret);
 }
