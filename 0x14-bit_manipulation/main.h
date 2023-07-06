c4c5439506f25f880439aa9f050d6ca442957af9
Joseph <jimonakedi@gmail.com>
2023-07-06 23:31:34 +0200

    fix putchar

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,7 @@
  *Desc: Header file for all functions written in the 0x14-bitmanipulation directory.
 */
 
-_putchar(char c);
+int _putchar(char c);
 unsigned int binary_to_unit(const char *b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
